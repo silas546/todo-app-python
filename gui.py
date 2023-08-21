3bff1b5889975aff58bddd61484d21e1da18c699
Silas-Owings <silas.owings@7summitsinc.com>
2023-08-21 11:09:17 -0500

    refactored static gui

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Enter a to-do")
 input_box = sg.InputText(tooltip="Type in a to-do")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[[label], [input_box, add_button]]])
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
 window.read()
 window.close()
